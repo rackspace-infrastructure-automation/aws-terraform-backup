fc5047a20ce22fe2a6b8081b5b99dffe1f655da2
bohn002 <nath@nbohn.com>
2019-06-17 16:57:57 -0500

    fixing test outputs

--- tests/backup/outputs.tf
+++ tests/backup/outputs.tf
@@ -1,29 +1,29 @@
 output "plan_arn" {
   description = "Plan ARN."
-  value       = "${module.plan.plan_arn}"
+  value       = "${module.backup_plan.plan_arn}"
 }
 
 output "plan_version" {
   description = "Unique, randomly generated, Unicode, UTF-8 encoded string that serves as the version ID of the backup plan."
-  value       = "${module.plan.plan_version}"
+  value       = "${module.backup_plan.plan_version}"
 }
 
 output "selection_id" {
   description = "Backup Selection identifier."
-  value       = "${module.selection.selection_id}"
+  value       = "${module.backup_plan.selection_id}"
 }
 
 output "vault_name" {
   description = "The name of the vault."
-  value       = "${module.vault.vault_name}"
+  value       = "${module.backup_plan.vault_name}"
 }
 
 output "vault_arn" {
   description = "The ARN of the vault."
-  value       = "${module.vault.vault_arn}"
+  value       = "${module.backup_plan.vault_arn}"
 }
 
 output "vault_recovery_points" {
   description = "The number of recovery points that are stored in a backup vault."
-  value       = "${module.vault.vault_recovery_points}"
+  value       = "${module.backup_plan.vault_recovery_points}"
 }
